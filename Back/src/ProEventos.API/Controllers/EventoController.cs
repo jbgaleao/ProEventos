ef10e6fb30e4704e42ceb4368a4ca14b2be782cf
Joao Galeao <jbgaleao@gmail.com>
2022-12-15 23:37:45 -0300

    aula 16

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -14,7 +14,26 @@ namespace ProEventos.API.Controllers
         [HttpGet]
         public string Get()
         {
-            return "value";
+            return "EXEMPLO DE GET";
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "EXEMPLO DE POST";
+        }
+
+        [HttpPut("{id}")]
+        public string Post(int id)
+        {
+            return $"EXEMPLO DE PUT COM ID = {id}";
+        }
+
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"EXEMPLO DE DELETE COM ID = {id}";
         }
     }
 }
